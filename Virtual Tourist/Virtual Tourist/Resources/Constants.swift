598323cb6c4b3ea431c31e7016a2ec4117948fab
Yevhen Herasymenko <fess2036@ya.ru>
2015-11-28 13:23:08 +0200

    feat: add navigation structure

---
+++ Virtual Tourist/Virtual Tourist/Resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Virtual Tourist
+//
+//  Created by Yevhen Herasymenko on 28/11/2015.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+import Foundation
+
+struct SegueConstants {
+    static let detailsSegue = "pinPhotosSegue"
+}
---
+++ Virtual Tourist/Virtual Tourist/ViewControllers/PinPhotosViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PinPhotosViewController.swift
+//  Virtual Tourist
+//
+//  Created by Yevhen Herasymenko on 28/11/2015.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+import UIKit
+
+class PinPhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
