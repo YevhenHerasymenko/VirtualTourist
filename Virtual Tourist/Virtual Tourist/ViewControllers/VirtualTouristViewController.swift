cbd44fe47341ae76eb2bde2174c608c41100be3d
Yevhen Herasymenko <fess2036@ya.ru>
2015-11-29 14:55:44 +0200

    feat: add gesture recognizers

--- Virtual Tourist/Virtual Tourist/ViewControllers/VirtualTouristViewController.swift
+++ Virtual Tourist/Virtual Tourist/ViewControllers/VirtualTouristViewController.swift
@@ -9,25 +9,57 @@
 import UIKit
 import MapKit
 
-class VirtualTouristViewController: UIViewController {
+class VirtualTouristViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var rightItem: UIBarButtonItem!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var heightBottomViewConstraint: NSLayoutConstraint!
     
+    var longPressAddPinRecognizer: UILongPressGestureRecognizer!
+    var tapRemovePinRecognizer: UITapGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         heightBottomViewConstraint.constant = 0
+        setupRecognizer()
+    }
+    
+    //MARK: - Recognizer
+    
+    func setupRecognizer() {
+        longPressAddPinRecognizer = UILongPressGestureRecognizer(target: self, action: "longPressAddPinRecognizer:")
+        tapRemovePinRecognizer = UITapGestureRecognizer(target: self, action: "tapRemovePinRecognizer:")
+        mapView.addGestureRecognizer(longPressAddPinRecognizer)
+    }
+    
+    func longPressAddPinRecognizer(recognizer: UIGestureRecognizer) {
+        if recognizer.state != UIGestureRecognizerState.Began {
+            return
+        }
+        
+        let touchPoint: CGPoint = recognizer.locationInView(mapView)
+        let touchMapCoordinate = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        let annotation: MKPointAnnotation = MKPointAnnotation()
+        annotation.coordinate = touchMapCoordinate
+        mapView.addAnnotation(annotation)
+    }
+    
+    func tapRemovePinRecognizer(recognizer: UIGestureRecognizer) {
+        
     }
     
     //MARK: - Actions
 
     @IBAction func edit(sender: UIBarButtonItem) {
+        mapView.removeGestureRecognizer(longPressAddPinRecognizer)
+        mapView.addGestureRecognizer(tapRemovePinRecognizer)
         heightBottomViewConstraint.constant = 60
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .Done, target: self, action: "done:")
     }
     
     @IBAction func done(sender: UIBarButtonItem) {
+        mapView.removeGestureRecognizer(tapRemovePinRecognizer)
+        mapView.addGestureRecognizer(longPressAddPinRecognizer)
         heightBottomViewConstraint.constant = 0
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .Plain, target: self, action: "edit:")
     }
