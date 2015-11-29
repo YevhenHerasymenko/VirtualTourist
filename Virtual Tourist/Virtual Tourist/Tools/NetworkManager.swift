3c636561344f3e86a1acec9b73c67771865280a7
Yevhen Herasymenko <fess2036@ya.ru>
2015-11-29 19:00:09 +0200

    feat: add network manager

--- Virtual Tourist/Virtual Tourist/Tools/NetworkManager.swift
+++ Virtual Tourist/Virtual Tourist/Tools/NetworkManager.swift
@@ -10,4 +10,20 @@ import Foundation
 
 class NetworkManager {
     static let sharedInstance = NetworkManager()
+    
+    func getPhotos(longitude: Double, latitude: Double)  {
+        
+    }
+    
+    //MARK: - Parameters
+    
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        var urlVars = [String]()
+        for (key, value) in parameters {
+            let stringValue = "\(value)"
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            urlVars += [key + "=" + "\(escapedValue!)"]
+        }
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
 }
